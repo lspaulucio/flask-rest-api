339339f95a57b8474d10364587d9b5b24672684d
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2022-12-03 15:55:13 -0300

    feat: Adjusting folders and gitignore

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Blueprint, Flask
 from flask_restx import Resource, Api
 from flask_mongoengine import MongoEngine
 
-from controllers.userController import UsersResource, UserResource
+from controllers.usersController import api as users_ns
+
 
 app = Flask(__name__)
+blueprint = Blueprint('api', __name__)
 
 app.config['MONGODB_SETTINGS'] = {
     'db': 'flaskAPI',
@@ -14,19 +16,11 @@ app.config['MONGODB_SETTINGS'] = {
     'password': 'admin'
 }
 
-api = Api(app)
+api = Api(app, title='Flask REST API', version='1.0',
+          description='Simple REST API built in python using flask framework', prefix='/api/v1')
 db = MongoEngine(app)
 
-
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-
-api.add_resource(UsersResource, '/users')
-api.add_resource(UserResource, '/users/<string:cpf>')
-api.add_resource(UserResource, '/user')
+api.add_namespace(users_ns, path='/users')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
