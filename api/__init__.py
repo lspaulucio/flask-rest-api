8e77a43b79ef2751443da690668ab3ea332e1690
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2022-12-23 14:07:39 -0300

    feat: adding prometheus and grafana to monitoring

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,10 @@
 from flask_restx import Api
-from flask import Blueprint, Flask
+from flask import Blueprint, Flask, Response
 from .database import init_db
 from .controllers.usersController import api as users_ns
+from prometheus_client.core import CollectorRegistry
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST
 
 
 def create_app(config):
@@ -9,6 +12,9 @@ def create_app(config):
     blueprint = Blueprint('api', __name__)
     app.register_blueprint(blueprint)
     app.config.from_object(config)
+
+    registry = CollectorRegistry()
+    metrics = PrometheusMetrics.for_app_factory(registry=registry)
 
     api = Api(app, title='Flask REST API',
               version='1.0',
@@ -19,5 +25,12 @@ def create_app(config):
     init_db(app)
 
     api.add_namespace(users_ns, path='/users')
+
+    metrics.init_app(app)
+
+    @app.route('/metrics')
+    def metrics():
+        data = generate_latest(registry)
+        return Response(data, mimetype=CONTENT_TYPE_LATEST)
 
     return app
